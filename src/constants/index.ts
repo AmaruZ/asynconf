494008abf97efa5d41bcd817e6391aab8cec051d
AmaruZ <a.zirouel@gmail.com>
2023-10-30 14:26:11 +0100

    First commit

---
+++ src/constants/index.ts
@@ -0,0 +1,30 @@
+export const vehiculeTypes = [
+    { type: 'Citadine', rate: '8' },
+    { type: 'Cabriolet', rate: '6' },
+    { type: 'Berline', rate: '6.5' },
+    { type: 'SUV / 4x4', rate: '4' },
+]
+
+export const energyTypes = [
+    { type: 'Essence', rate: '5' },
+    { type: 'Electrique', rate: '9' },
+    { type: 'Gaz', rate: '6' },
+    { type: 'Diesel', rate: '4' },
+    { type: 'Hybride', rate: '7' },
+]
+
+export const mileageOptions = [
+    { type: '5-10K/km', rate: '9' },
+    { type: '10-15K/km', rate: '7' },
+    { type: '15-20K/km', rate: '5' },
+    { type: '20-25K/km', rate: '3' },
+    { type: '25-30K/km', rate: '1' },
+]
+
+export const yearOptions = [
+    { type: '1960-1970', rate: '1' },
+    { type: '1970-1980', rate: '2' },
+    { type: '1990-2000', rate: '4' },
+    { type: '2000-2010', rate: '5' },
+    { type: 'Après 2010', rate: '7' },
+]
